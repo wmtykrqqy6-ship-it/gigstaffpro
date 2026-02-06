4168ac4878bd6c23d683fc1f4586be532421e04f
wmtykrqqy6-ship-it <wmtykrqqy6@privaterelay.appleid.com>
2026-02-05 21:35:16 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6391,5 +6391,24 @@ setPositions(storedPositions);
         <p className="text-gray-600">This feature will be available soon!</p>
       </div>
     );
-  };
+  };return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <Navigation />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {renderView()}
+      </div>
+      <NotificationsModal />
+      <AddWorkerModal />
+      <BulkInviteModal />
+      <SetPinModal />
+      <EditWorkerModal />
+      <AddEventModal />
+      <EditEventModal />
+      <AssignWorkersModal />
+      <PaymentCalculatorModal />
+    </div>
+  );
+};
+
 export default GigStaffPro;
