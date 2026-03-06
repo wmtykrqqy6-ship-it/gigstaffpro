35b0711200d167afb9255e5105ac9739d8cc0ea8
wmtykrqqy6-ship-it <wmtykrqqy6@privaterelay.appleid.com>
2026-03-06 10:17:11 -0600

    Update send-email.js

--- API/send-email.js
+++ API/send-email.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        from: 'Vegas on Wheels <noreply@vegasonwheels.com>',
+        from: 'Vegas on Wheels <noreply@gigstaffpro.com>',
         to: [to],
         subject,
         html
